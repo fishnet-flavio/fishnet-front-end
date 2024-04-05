81f4707f0f24d13bcbea2b20f8d0a28d9ef92067
Leonardo Tironi <135834266+LeoTironi@users.noreply.github.com>
2024-04-05 16:58:30 -0300

    Add files via upload
    Signin Frufru 💅

--- src/components/molecule/SignInCard/index.tsx
+++ src/components/molecule/SignInCard/index.tsx
@@ -23,18 +23,19 @@ const SignInCard = () => {
                     <BaseImage src={logo} $width="12rem" $height="10rem" />
                 </Link>
                 <BaseText $fontSize={24} $fontWeight="bold">Registre-se</BaseText>
-                <Base $flexDirection="row" $gap={2} $alignItems="center">
+                <Base $flexDirection="row" $gap={2} $alignItems="center" $justifyContent="space-between">
                     <BaseText $fontSize={20} >Nome: </BaseText>
-                    <BaseInput $width="100%" value={username} onChange={(event) => setUsername(event.target.value)} placeholder="Nome de usuário" />
+                    <BaseInput $width="17.5rem" value={username} onChange={(event) => setUsername(event.target.value)} placeholder="Nome de usuário" />
                 </Base>
-                <Base $flexDirection="row" $gap={2} $alignItems="center">
-                    <BaseText $fontSize={20} >Email: </BaseText>
-                    <BaseInput $width="100%" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email" />
+                <Base $flexDirection="row" $gap={2} $alignItems="center" $justifyContent="space-between">
+                    <BaseText $fontSize={20} style={{justifySelf: "start", backgroundColor:""}}>Email: </BaseText>
+                    <BaseInput $width="17.5rem"  value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email" />
                 </Base>
-                <Base $flexDirection="row" $gap={2} $alignItems="center">
+                <Base $flexDirection="row" $gap={2} $alignItems="center" $justifyContent="space-between">
                     <BaseText $fontSize={20}>Senha: </BaseText>
-                    <BaseInput $width="100%" placeholder="Senha" type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+                    <BaseInput $width="17.5rem" placeholder="Senha" type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
                 </Base>
+
                 <Base $flexDirection="row" $gap={2} $alignItems="center">
                     <BaseText $fontSize={20} >Icone: </BaseText>
                     <BaseInput type="file" />
