e415aaaf7b0f40b72b3cb5a9869010b4b5ecda10
Aluno <natan.br15@gmail.com>
2024-08-16 10:28:22 -0300

    :wrench: feat: change form to use form hook

--- src/pages/ProductRegisterPage/index.tsx
+++ src/pages/ProductRegisterPage/index.tsx
@@ -6,11 +6,14 @@ import BaseButton from "../../components/atom/BaseButton"
 import { SyntheticEvent } from "react"
 import handleProductRegister from "./handleProductRegister"
 import { z } from "zod"
+import { useForm } from "react-hook-form"
 
 const ProductRegisterPage = () => {
     const params = useParams()
 
-    const createProductFormSchema = z.object({
+    const { register } = useForm();
+
+    const productFormSchema = z.object({
         name: z
         .string()
         .min(1, "Nome não pode ser vazio"),
@@ -36,28 +39,28 @@ const ProductRegisterPage = () => {
 
     return (
         <Base $alignItems="center" $background="transparent">
-            <Base $boxShadow $borderRadius={12} $gap={2} $alignItems="center" $padding="1rem 2rem" $width="max-content">
+            <Base $borderRadius={12} $gap={2} $alignItems="center" $padding="1rem 2rem" $width="40%">
                 <BaseText $fontSize={24} $fontWeight="bold">Registro de Produto</BaseText>
                 <form onSubmit={handleSubmit}>
                     <label>
-                        <BaseText>Nome do produto: </BaseText>
+                        <BaseText>Nome: </BaseText>
                     </label>
-                        <BaseInput $width="90%" placeholder="Nome do produto"/>
+                        <BaseInput $width="90%" $margin=".5rem 0" placeholder="Nome do produto" {...register("name")}/>
                     <label>
-                        <BaseText>Preço do produto: </BaseText>
+                        <BaseText>Preço: </BaseText>
                     </label>
-                        <BaseInput $width="90%" type="number" placeholder="Preço do produto"/>
+                        <BaseInput $width="90%" $margin=".5rem 0" type="number" placeholder="Preço do produto" {...register("price")}/>
                     <label>
-                        <BaseText>Descrição do produto: </BaseText>
+                        <BaseText>Descrição: </BaseText>
                     </label>
-                        <BaseInput $width="90%" placeholder="Descrição do produto"/>
+                        <BaseInput $width="90%" $margin=".5rem 0" placeholder="Descrição do produto" {...register("description")}/>
                     <label>
                         <BaseText>Estoque: </BaseText>
                     </label>
-                        <BaseInput $width="90%" type="number" placeholder="Estoque"/>
+                        <BaseInput $width="90%" $margin=".5rem 0" type="number" placeholder="Estoque" {...register("stock")}/>
                     <label>
-                        <BaseText>Imagem do Produto: </BaseText>
-                        <BaseInput $width="90%" type="file" accept="image/png, image/jpeg"/>
+                        <BaseText>Imagem: </BaseText>
+                        <BaseInput $width="90%" $margin=".5rem 0" type="file" accept="image/png, image/jpeg" {...register("image")}/>
                     </label>
                     <BaseButton type="submit">Enviar</BaseButton>
                 </form>
